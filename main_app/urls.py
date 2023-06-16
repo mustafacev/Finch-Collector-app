2495459b1de43b2a069a4e3af86e14ca6195252b
mustafacev <mustafaorcuncevik@gmail.com>
2023-06-16 19:28:31 -0400

    views created

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 # this like app.use() in express
 urlpatterns = [
-
+ path('', views.Home.as_view(), name='home'),
+ path('about/', views.About.as_view(), name='about'),
+ path('cars/', views.ArtistList.as_view(), name="car_list"),
 ]
