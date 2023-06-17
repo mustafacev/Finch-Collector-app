d116a0249726f1bb0f3b1815fc9eb4772edf3453
mustafacev <mustafaorcuncevik@gmail.com>
2023-06-16 20:58:56 -0400

    home page created

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
  path('', views.Home.as_view(), name='home'),
- path('about/', views.About.as_view(), name='about'),
- path('cars/', views.ArtistList.as_view(), name="car_list"),
+#  path('about/', views.About.as_view(), name='about'),
+#  path('cars/', views.ArtistList.as_view(), name="car_list"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponse
 # Create your views here.
+
+class Home(View):
+      def get(self, request):
+        # Here we are returning a generic response
+        # This is similar to response.send() in express
+        return HttpResponse("Cars Home page")
