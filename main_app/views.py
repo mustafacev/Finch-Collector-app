64593c7bc739660ebb3a4716ad786e5c4c1ab81e
mustafacev <mustafaorcuncevik@gmail.com>
2023-06-21 18:37:33 -0400

    templates edited

--- main_app/views.py
+++ main_app/views.py
@@ -6,8 +6,5 @@ from django.views.generic.base import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
-
-class About(View):
-
-    def get(self, request):
-        return HttpResponse("Cars About")
+class About(TemplateView):
+    template_name = "about.html"
