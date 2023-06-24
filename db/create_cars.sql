637e651cab57d9f1adbc0f8e709e2f3472b32ed1
mustafacev <mustafaorcuncevik@gmail.com>
2023-06-24 17:52:55 -0400

    edited data model and view

---
+++ db/create_cars.sql
@@ -0,0 +1,32 @@
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Porsche','https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-t-021-dsc04152-1669058415.jpg?crop=0.596xw:0.502xh;0.216xw,0.377xh&resize=1200:*','AC/DC''s mammoth power-chord roar became one of the most influential hard rock sounds of the ''70s and one of the defining sounds of rock and metal. In its own way, it was a reaction to the pompous art rock and lumbering arena rock of the early ''70s. AC/DC''s rock was minimalist -- no matter how huge and bludgeoning their guitar chords were, there was a clear sense of space and restraint.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Toyota','https://toyota-cms-media.s3.amazonaws.com/wp-content/uploads/2022/10/2023-Corolla-Sedan_001H-1500x900.jpg','By the time Bella Thorne started recording her first album, Call It Whatever, in 2013 at the age of 16, she already had a long career in showbiz -- a career that began long before she became part of the Disney Channel universe in 2010, when she landed a starring role in the sitcom Shake It Up.','f','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Toyota','https://toyota-cms-media.s3.amazonaws.com/wp-content/uploads/2022/10/2023-Corolla-Sedan_001H-1500x900.jpg','Cher is an American singer, actress and television personality. Commonly referred to by the media as the "Goddess of Pop", she has been described as embodying female autonomy in a male-dominated industry.','f','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Porsche','https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-t-021-dsc04152-1669058415.jpg?crop=0.596xw:0.502xh;0.216xw,0.377xh&resize=1200:*','Cyndi Lauper is a groundbreaking Grammy, Emmy, and Tony Award-winning songwriter and performing cars with global record sales in excess of 50 million. Her iconic voice, influential punk glamour, and infectious live shows have catapulted her to stardom. Lauper won the Grammy Award for Best New cars with her first album, She''s So Unusual, and became the first woman in history to have four top-five singles from a debut album.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Porsche','https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-t-021-dsc04152-1669058415.jpg?crop=0.596xw:0.502xh;0.216xw,0.377xh&resize=1200:*','Gorillaz are once again disrupting the paradigm and breaking convention in their round the back door fashion with Song Machine, the newest concept from one of the most inventive bands around.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Porsche','https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-t-021-dsc04152-1669058415.jpg?crop=0.596xw:0.502xh;0.216xw,0.377xh&resize=1200:*','Joji is one of the most enthralling cars of the digital age. New album Nectar arrives as an eagerly anticipated follow-up to Joji''s RIAA Gold-certified first full-length album BALLADS 1, which topped the Billboard R&B / Hip-Hop Charts and has amassed 3.6B+ streams to date.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Porsche','https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-t-021-dsc04152-1669058415.jpg?crop=0.596xw:0.502xh;0.216xw,0.377xh&resize=1200:*','In the beginning there was light… and darkness. A creation of perfect balance. As time passed, evil spread over the land like a plague, slowly consuming everything in its path. In the final moments before light was lost to the shadow for all time, a weapon of infinite power was created, the Magic Sword.','f','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_cars(name, img, bio, verified_cars, created_at)
+  VALUES
+    ('Metallica','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSooXDY745RR9Mj8hUqzH0_HWPJ4ZwbYo4ll_ucN7lVpqEMogOBKG3AH6_hPwm8ILXdoik&usqp=CAU','Metallica formed in 1981 by drummer Lars Ulrich and guitarist and vocalist James Hetfield and has become one of the most influential and commercially successful rock bands in history, having sold 110 million albums worldwide while playing to millions of fans on literally all seven continents.','t','2021-05-07 18:57:42.517205-07');
+
--- main_app/views.py
+++ main_app/views.py
@@ -6,48 +6,55 @@ from django.views.generic.base import TemplateView
 from .models import Cars
 # Create your views here.
 
+
 class Home(TemplateView):
     template_name = "home.html"
+
+
 class About(TemplateView):
     template_name = "about.html"
 
- #adds artist class for mock database data
-class Cars:
-    def __init__(self, name, image, info):
-        self.name = name
-        self.image = image
-        self.info = info
+ # adds artist class for mock database data
 
 
-cars = [
-  Cars("Toyota Corolla", "https://toyota-cms-media.s3.amazonaws.com/wp-content/uploads/2022/10/2023-Corolla-Sedan_001H-1500x900.jpg",
-          "Brand new car."),
-  Cars("Honda Civic",
-          "https://hips.hearstapps.com/hmg-prod/images/2022-honda-civic-hatchback-sport-touring-309-1634066512.jpg?crop=0.617xw:0.520xh;0.298xw,0.477xh&resize=1200:*", " 2023 Honda Civic   ."),
-  Cars("Mazda ", "https://cdn.centraljersey.com/wp-content/uploads/sites/26/2023/01/2022-Mazda-3-Sedan-696x392.jpg",
-          "2022 Mazda 3 Turbo AWD."),
-  Cars("Hyundai",
-          "https://hips.hearstapps.com/hmg-prod/images/2024-hyundai-elantra-n-106-643eb61f5ee7c.jpg?crop=1.00xw:0.567xh;0,0.290xh&resize=1200:*", "2023 Hyundai Elentra."),
-  Cars("Jeep",
-          "https://pictures.dealer.com/g/gengrascdjrfairfieldcllc/0494/cd98dac15382037b7841670a4c0868c8x.jpg", "2023 Wrangler "),
-  Cars("Porsche",
-          "https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-t-021-dsc04152-1669058415.jpg?crop=0.596xw:0.502xh;0.216xw,0.377xh&resize=1200:*", "2023 Porsche starting at $100k."),
-]
+# class Car:
+#     def __init__(self, name, image,bio):
+#         self.name = name
+#         self.image = image
+#         self.bio = bio
+
+
+# cars = [
+#     Cars("Toyota Corolla", "https://toyota-cms-media.s3.amazonaws.com/wp-content/uploads/2022/10/2023-Corolla-Sedan_001H-1500x900.jpg",
+#          "Brand new car."),
+#     Cars("Honda Civic",
+#          "https://hips.hearstapps.com/hmg-prod/images/2022-honda-civic-hatchback-sport-touring-309-1634066512.jpg?crop=0.617xw:0.520xh;0.298xw,0.477xh&resize=1200:*", " 2023 Honda Civic   ."),
+#     Cars("Mazda ", "https://cdn.centraljersey.com/wp-content/uploads/sites/26/2023/01/2022-Mazda-3-Sedan-696x392.jpg",
+#          "2022 Mazda 3 Turbo AWD."),
+#     Cars("Hyundai",
+#          "https://hips.hearstapps.com/hmg-prod/images/2024-hyundai-elantra-n-106-643eb61f5ee7c.jpg?crop=1.00xw:0.567xh;0,0.290xh&resize=1200:*", "2023 Hyundai Elentra."),
+#     Cars("Jeep",
+#          "https://pictures.dealer.com/g/gengrascdjrfairfieldcllc/0494/cd98dac15382037b7841670a4c0868c8x.jpg", "2023 Wrangler "),
+#     Cars("Porsche",
+#          "https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-t-021-dsc04152-1669058415.jpg?crop=0.596xw:0.502xh;0.216xw,0.377xh&resize=1200:*", "2023 Porsche starting at $100k."),
+# ]
+
 
 class CarsList(TemplateView):
     template_name = "cars_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # to get the query parameter we have to acccess it in the request.GET dictionary object        
+        # to get the query parameter we have to acccess it in the request.GET dictionary object
         name = self.request.GET.get("name")
-        # If a query exists we will filter by name 
+        # If a query exists we will filter by name
         if name != None:
             # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
-            context["cars"] = Cars.objects.filter(name__icontains=name)
+            context["Cars"] = Cars.objects.filter(name__icontains=name)
         else:
-            context["cars"] = Cars.objects.all()
+            context["Cars"] = Cars.objects.all()
         return context
+
 
 class ModelList(TemplateView):
     template_name = "model_list.html"
@@ -66,5 +73,5 @@ class Model:
 
 models = [
     Model("Toyota Tundra", "4x4"),
-    Model("Ford F-150","4x4")
+    Model("Ford F-150", "4x4")
 ]
